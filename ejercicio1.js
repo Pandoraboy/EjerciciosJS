e07d8307a4d296b0d73c7e8e7e0fc894dbadecff
Pandora <nicolas.jimenez2001@alumnos.ubiobio.cl>
2024-09-15 20:49:33 -0300

    Primer ejercicio

---
+++ ejercicio1.js
@@ -0,0 +1,10 @@
+let nombre = "Nicolas"
+console.log(nombre)
+console.log(typeof nombre)
+let edad = 22
+console.log(edad)
+console.log(typeof edad)
+let puedoConducir = false
+console.log(puedoConducir)
+console.log(typeof puedoConducir)
+
