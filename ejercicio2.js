71ad4d91c82b8e88feebce2df0a7c7dcbf284716
Pandora <nicolas.jimenez2001@alumnos.ubiobio.cl>
2024-09-15 21:01:42 -0300

    Segundo ejercicio

---
+++ ejercicio2.js
@@ -0,0 +1,17 @@
+let numero1 = 20
+let numero2 = 40
+
+let suma = numero1 + numero2
+console.log(suma)
+
+let resta = numero1 - numero2
+console.log(resta)
+
+let multiplicacion = numero1 * numero2
+console.log(multiplicacion)
+
+let division = numero1 / numero2
+console.log(division)
+
+let modulo = numero1 % numero2
+console.log(modulo)
